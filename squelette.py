6d383e9bde36a99ea573430c9bfce1cdfb6ba116
Xylooooooo <etiennelopezvilespy@gmail.com>
2026-02-08 22:43:19 +0100

    Ajout : venv + initialisation param

---
+++ squelette.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def initialisation_parametres(dimension_couches):
+    """
+    Arguments:
+    dimension_couches -- dimension des couches
+    
+    returns:
+    parametres -- dictionnaire des paramètres initialisés
+    """
+
+    np.random.seed(1)
+    L = len(dimension_couches)
+    parametres = {}
+
+    for couche in range (1, L):
+        parametres[f"W{couche}"] = np.random.randn(dimension_couches[couche], dimension_couches[couche - 1]) * 0.01
+        parametres[f"b{couche}"] = np.zeros((dimension_couches[couche], 1))
+    
+    return parametres
+
+#faire he init
