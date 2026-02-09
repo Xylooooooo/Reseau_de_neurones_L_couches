d1cf9f1b7d2afae276c358284757ef5acab8fdf7
Xylooooooo <etiennelopezvilespy@gmail.com>
2026-02-09 21:05:50 +0100

    he init

--- squelette.py
+++ squelette.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def initialisation_parametres(dimension_couches):
+def initialisation_parametres_classique(dimension_couches):
     """
     Arguments:
     dimension_couches -- dimension des couches
@@ -19,4 +19,22 @@ def initialisation_parametres(dimension_couches):
     
     return parametres
 
-#faire he init
+
+def initialisation_parametres_he(dimension_couches):
+    """
+    Arguments:
+    dimension_couches -- dimension des couches
+    
+    returns:
+    parametres -- dictionnaire des paramètres initialisés
+    """
+
+    np.random.seed(1)
+    L = len(dimension_couches)
+    parametres = {}
+
+    for couche in range (1, L):
+        parametres[f"W{couche}"] = np.random.randn(dimension_couches[couche], dimension_couches[couche - 1]) * np.sqrt(2 / dimension_couches[couche - 1])
+        parametres[f"b{couche}"] = np.zeros((dimension_couches[couche], 1))
+    
+    return parametres
